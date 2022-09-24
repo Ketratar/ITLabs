282e87d00dcf45508b3176910137678a68cd22cd
Ketratar <53187132+Ketratar@users.noreply.github.com>
2022-09-24 12:20:55 +0300

    Site files

---
+++ script/script.js
@@ -0,0 +1,35 @@
+function lab1f(){
+	var x = document.getElementById("info1");
+	if(x.style.display === "none")
+	{
+		x.style.display = "block";
+	}
+	else
+	{
+		x.style.display = "none";
+	}
+}
+
+function lab2f(){
+	var x = document.getElementById("info2");
+	if(x.style.display === "none")
+	{
+		x.style.display = "block";
+	}
+	else
+	{
+		x.style.display = "none";
+	}
+}
+
+function lab3f(){
+	var x = document.getElementById("info3");
+	if(x.style.display === "none")
+	{
+		x.style.display = "block";
+	}
+	else
+	{
+		x.style.display = "none";
+	}
+}
